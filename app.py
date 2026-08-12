cf95eab00ec96e929c7657a4ba6288a22dbd20fa
ozlove7977-web <ozlove7977@gmail.com>
2026-08-12 19:01:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,19 +32,19 @@ AUTO_RETURN_SECONDS = 17
 
 LESSONS = [
     {
-        "name": "과외 1",
+        "name": "수학 과외",
         "english": "SESSION 01",
-        "minutes": 1,   # 테스트 후 실제 시간으로 변경
+        "minutes": 90,   # 테스트 후 실제 시간으로 변경
     },
     {
-        "name": "과외 2",
+        "name": "영어 과외",
         "english": "SESSION 02",
         "minutes": 90,
     },
     {
-        "name": "과외 3",
+        "name": "첼로 과외",
         "english": "SESSION 03",
-        "minutes": 120,
+        "minutes": 50,
     },
 ]
 
